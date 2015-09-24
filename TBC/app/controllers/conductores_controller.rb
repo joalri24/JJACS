26d2f9200933890cbc5b952062c57ad5494ed962
anvas380 <a.navas380@gmail.com>
2015-09-24 12:26:53 -0500

    cambios

--- TBC/app/controllers/conductores_controller.rb
+++ TBC/app/controllers/conductores_controller.rb
@@ -16,11 +16,25 @@ class ConductoresController < ApplicationController
   end
 
   def crear
+    @conductor = Conductor.new(conductor_params)
+
+    respond_to do |format|
+      if @conductor.save
+        format.html { redirect_to @conductor, notice: 'Conductor was successfully created.' }
+        format.json { render :mostrar, status: :created, location: @conductor }
+      else
+        format.html { render :nuevo }
+        format.json { render json: @conductor.errors, status: :unprocessable_entity }
+      end
+    end
+
   end
 
   def actualizar
   end
 
   def destruir
+
   end
+
 end
