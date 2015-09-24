9c3211b4ee3822d2aab5d7223530b6bf8ba7275d
anvas380 <a.navas380@gmail.com>
2015-09-24 14:50:23 -0500

    mobibus controller

---
+++ TBC/app/controllers/mobibuses_controller.rb
@@ -0,0 +1,16 @@
+class MobibusesController < ApplicationController
+  def crear
+  end
+
+  def actualizar
+  end
+
+  def destruir
+  end
+
+  def index
+  end
+
+  def mostrar
+  end
+end
---
+++ TBC/test/controllers/mobibuses_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class MobibusesControllerTest < ActionController::TestCase
+  test "should get crear" do
+    get :crear
+    assert_response :success
+  end
+
+  test "should get actualizar" do
+    get :actualizar
+    assert_response :success
+  end
+
+  test "should get destruir" do
+    get :destruir
+    assert_response :success
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get mostrar" do
+    get :mostrar
+    assert_response :success
+  end
+
+end
