57312c9c8da66a34b05c9f1ba6bf5d6aab797609
anvas380 <a.navas380@gmail.com>
2015-09-24 11:57:56 -0500

    primer modelo

---
+++ TBC/db/migrate/20150924165526_create_conductors.rb
@@ -0,0 +1,11 @@
+class CreateConductors < ActiveRecord::Migration
+  def change
+    create_table :conductors do |t|
+      t.integer :cedula
+      t.string :nombre
+      t.float :puntaje
+
+      t.timestamps
+    end
+  end
+end
