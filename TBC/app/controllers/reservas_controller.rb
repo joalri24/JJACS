7b62a5b202c13e5578d3349c5cded0c6caba7e41
anvas380 <a.navas380@gmail.com>
2015-10-02 22:54:44 -0500

    reservas

--- TBC/app/controllers/reservas_controller.rb
+++ TBC/app/controllers/reservas_controller.rb
@@ -1,17 +1,30 @@
 class ReservasController < ApplicationController
 
-    def crear
-      @reserva= Reserva.new
-      @Reserva.update_attributes(estado:0, direccion_origen: params[:direccion_origen], direccion_destino: params[:direccion_destino], cliente_id:  params[:cliente_id], mobibus_id:0, fecha: paramas[:fecha] )
-      @reserva.save
-      respond_to do |format|
-        format.html { redirect_to action:'mostrar', id:@reserva.id , status: 200}
-      end
+  before_action :set_reserva, only: [:mostrar]
+  def crear
+    @hora= params[:hora].to_s
+    @fecha = params[:fecha]
+    @fechaf= @fecha + "" +@hora
+    date = DateTime.parse(@fechaf)
+    @formatted_date = date.strftime('%b %d %Y %H:%M:%S')
+    @reserva =Reserva.create(estado:0, direccion_origen: params[:direccion_origen], direccion_destino: params[:direccion_destino], cliente_id:  params[:cliente_id], mobibus_id:0, fecha: @formatted_date)
+    @reserva.save
+    respond_to do |format|
+      format.html { redirect_to action:'mostrar', id: @reserva.id, status: 200}
     end
+
+  end
 
     def mostrar
+
+
     end
 
+
+  def set_reserva
+
+    @reserva= Reserva.find(params[:id])
+  end
 
 
 end
